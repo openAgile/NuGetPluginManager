9346669d4b802ff218f2f4fc5794471dad54b0dc
Josh Gough <jsgough@gmail.com>
2014-03-19 14:08:06 -0400

    Started test project and updated to fixed version

---
+++ NuGetPluginManager.fs
@@ -0,0 +1,128 @@
+﻿namespace OpenAgile.NuGetPluginManager
+
+open System
+open NuGet
+open System.Collections.Generic
+open System.Linq
+open System.IO
+
+type PackageModel = {
+    Id : String
+    VersionString : String
+    SemanticVersion: NuGet.SemanticVersion
+    Description : String
+    IsInstalled : bool
+    ReleaseNotes : String
+    Authors : String
+}
+
+type PackageInstalledModel = {
+    Id : String
+    Version : String
+    IsInstalled : bool
+}
+
+
+type NuGetPluginManager(packageFolder, installFolder, packageSearchPattern
+, remotePackageManager : IPackageManager, localPackageManager : IPackageManager) =
+    let installMarker packageId packageVersion = installFolder + "\\" + packageId + "." + packageVersion + ".installed"
+
+    let isInstalledInInstallFolder packageId packageVersion =
+        File.Exists(installFolder + "\\" + packageId + ".dll") && File.Exists(installMarker packageId packageVersion)
+               
+    let copyModuleToInstallFolder packageId packageVersion = 
+        let searchPattern = packageId + ".dll"
+        let moduleFolder = DirectoryInfo (packageFolder + "\\" + packageId + "." + packageVersion)
+        for file in moduleFolder.GetFiles(searchPattern, SearchOption.AllDirectories) do
+            let destinationFile = (installFolder + "\\" + file.Name)
+            file.CopyTo(destinationFile, true) |> ignore
+            File.Create(installMarker packageId packageVersion).Dispose()
+
+    let deletePreviousInstallMarkers packageId = 
+        let searchPattern = packageId + "*.installed"
+        let installFolderFiles = DirectoryInfo(installFolder)
+        for file in installFolderFiles.GetFiles(searchPattern) do
+            file.Delete()
+
+    let deleteModuleFromInstallFolder packageId packageVersion =        
+        let searchPattern = packageId + ".dll"        
+        let installFiles = DirectoryInfo installFolder
+        for file in installFiles.GetFiles searchPattern do 
+            file.Delete()
+            File.Delete(installMarker packageId packageVersion)
+        ()
+
+    member x.ListPackages () =
+        let allPackages = [ 
+            for repoPackage in remotePackageManager.SourceRepository.GetPackages() ->
+                { 
+                    Id = repoPackage.Id;
+                    VersionString = repoPackage.Version.ToString();
+                    SemanticVersion = repoPackage.Version;
+                    Description = repoPackage.Description;
+                    IsInstalled = (isInstalledInInstallFolder repoPackage.Id (repoPackage.Version.ToString()) );
+                    ReleaseNotes = repoPackage.ReleaseNotes;
+                    Authors = String.Join("",repoPackage.Authors.ToArray())
+                }
+        ]
+        let groups = query {
+            for p in allPackages do
+            groupBy p.Id
+        }        
+        let groupedByIdSortedByVersion = Dictionary<String, IOrderedEnumerable<PackageModel>>()
+        for group in groups do            
+            groupedByIdSortedByVersion.Add(group.Key, group.ToList().OrderByDescending(fun g -> g.SemanticVersion))
+        groupedByIdSortedByVersion
+
+    member x.ListLatestPackagesByPattern =
+        x.ListLatestPackageByPattern packageSearchPattern
+
+    member x.ListLatestPackageByPattern pattern =
+        let allPackages = [ 
+            for repoPackage in remotePackageManager.SourceRepository.Search(pattern, false) ->
+                { 
+                    Id = repoPackage.Id;
+                    VersionString = repoPackage.Version.ToString();
+                    SemanticVersion = repoPackage.Version;
+                    Description = repoPackage.Description;
+                    IsInstalled = (isInstalledInInstallFolder repoPackage.Id (repoPackage.Version.ToString()) );
+                    ReleaseNotes = repoPackage.ReleaseNotes;
+                    Authors = String.Join("",repoPackage.Authors.ToArray())
+                }
+        ]
+        let groups = query {
+            for p in allPackages do
+            groupBy p.Id
+        }        
+        let groupedByIdOnlyLatestVersion = Dictionary<String, PackageModel>()
+        for group in groups do            
+            groupedByIdOnlyLatestVersion.Add(group.Key, group.ToList().OrderByDescending(fun g -> g.SemanticVersion).First())
+        groupedByIdOnlyLatestVersion
+
+    member x.Install packageId (packageVersion : String) =
+        localPackageManager.InstallPackage(packageId, SemanticVersion(packageVersion), false, false)
+        deletePreviousInstallMarkers packageId
+        copyModuleToInstallFolder packageId packageVersion
+        { Id = packageId; Version = packageVersion; IsInstalled = true }
+
+    member x.Uninstall packageId (packageVersion : String) =
+        localPackageManager.UninstallPackage(packageId, SemanticVersion(packageVersion), false, false)
+        deleteModuleFromInstallFolder packageId packageVersion
+        { Id = packageId; Version = packageVersion; IsInstalled = false }
+
+    static member Create packageFolder installFolder packageSearchPattern remotePackage remotePackageManagerRepositoryUrl (additionalPackageRepositoryUrls : IEnumerable<string>) =    
+        let mainPackageRepository = NuGet.PackageRepositoryFactory.Default.CreateRepository(remotePackageManagerRepositoryUrl)
+        let remotePackageManager = NuGet.PackageManager(mainPackageRepository, packageFolder)
+
+        let additionalPackageRepositories = [
+            for packageRepositoryUrl in additionalPackageRepositoryUrls -> NuGet.PackageRepositoryFactory.Default.CreateRepository(packageRepositoryUrl)
+        ]
+        
+        let allRepositories = additionalPackageRepositories.Union([mainPackageRepository])
+
+        let allPackageRepositories = NuGet.AggregateRepository(allRepositories)
+        let localPackageManager = NuGet.PackageManager(allPackageRepositories, packageFolder)
+
+        NuGetPluginManager(packageFolder, installFolder, packageSearchPattern, remotePackageManager, localPackageManager)
+
+    
---
+++ OpenAgile.TickedOffTest/TickedOffTest.fs
@@ -0,0 +1,117 @@
+﻿namespace OpenAgile.TickedOffTest
+
+open System.Reflection
+open NUnit.Framework
+open TickSpec
+
+module internal TextReader =
+    open System.IO
+    /// Reads lines from TextReader
+    let ReadLines (reader:System.IO.TextReader) =       
+        seq {                  
+            let isEOF = ref false
+            while not !isEOF do
+                let line = reader.ReadLine()                
+                if line <> null then yield line
+                else isEOF := true
+        }
+    /// Read all lines to a string array
+    let ReadAllLines reader = reader |> ReadLines |> Seq.toArray  
+    
+[<AutoOpen>]
+module Parser =
+    let parse source =    
+        let ass = Assembly.GetExecutingAssembly()
+        let stream = ass.GetManifestResourceStream(source)
+        use reader = new System.IO.StreamReader(stream)
+        let lines = reader |> TextReader.ReadAllLines
+        TickSpec.FeatureParser.parseFeature(lines)
+
+[<AutoOpen>]
+module Exceptions =
+    exception Pending of unit
+    let pending () = raise <| Pending()
+    let notImplemented () = raise <| new System.NotImplementedException()
+
+[<AutoOpen>]
+module Patterns =
+    open System.Text.RegularExpressions
+
+    let Regex input pattern =
+        let r = Regex.Match(input,pattern)
+        if r.Success then Some [for i = 1 to r.Groups.Count-1 do yield r.Groups.[i].Value]
+        else None
+
+    let (|Given|_|) (pattern:string) (step) =
+        match step with
+        | GivenStep input -> Regex input pattern        
+        | WhenStep _ | ThenStep _ -> None
+
+    let (|When|_|) (pattern:string) (step) =
+        match step with
+        | WhenStep input -> Regex input pattern        
+        | GivenStep _ | ThenStep _ -> None    
+
+    let (|Then|_|) (pattern:string) (step) =
+        match step with
+        | ThenStep input -> Regex input pattern        
+        | GivenStep _ | WhenStep _ -> None
+
+    let (|Int|) s = System.Int32.Parse(s)
+
+
+open System
+open System.Net
+open System.Net.Http
+open Newtonsoft.Json.Linq
+
+[<AutoOpen>]
+module Helpers =
+  let makeA f a = async { let! x = a
+                          return f x }
+  let assertStatus s (resp:HttpResponseMessage) = Assert.AreEqual(s, resp.StatusCode)
+  let assertBadRequest respA =  respA |> (assertStatus HttpStatusCode.BadRequest |> makeA)
+  let assertNotFound respA =  respA |> (assertStatus HttpStatusCode.NotFound |> makeA)
+
+  let getGuid key (o:Newtonsoft.Json.Linq.JObject) = Guid(o.[key].ToString())
+  let ignoreA = Async.Ignore
+  let await = Async.AwaitTask
+  let start = Async.StartAsTask
+  let run = Async.RunSynchronously
+  let ensureJson (a:Async<HttpResponseMessage>) = async {
+    let! resp = a
+    let! body = resp.EnsureSuccessStatusCode()
+                    .Content
+                    .ReadAsStringAsync() |> await
+    return JObject.Parse body
+    }
+
+module Runner =
+    open System
+
+    let tryStep f acc (step,line) =
+        let print color =
+            let old = Console.ForegroundColor
+            Console.ForegroundColor <- color
+            printfn "%s" (line.Text.Trim())
+            Console.ForegroundColor <- old
+        try 
+            let acc = f acc (step,line)
+            print ConsoleColor.Green
+            acc
+        with e ->
+            print ConsoleColor.Red
+            printfn "%A" e
+            reraise ()
+
+    let run feature f init =
+        let feature = parse feature
+        feature.Scenarios
+        |> Seq.filter (fun scenario -> scenario.Tags |> Seq.exists ((=) "ignore") |> not) 
+        |> Seq.iter (fun scenario ->
+            scenario.Steps |> Array.scan (tryStep f) init
+            |> ignore
+        )
+        System.Console.ReadLine () |> ignore
+        
+    //do run "Addition.txt" (AdditionSteps.performStep) (Calculator.Create ())
