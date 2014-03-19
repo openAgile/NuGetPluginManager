4690116aa74149382c6b1f5b82ba81583587654b
Josh Gough <jsgough@gmail.com>
2014-03-19 19:39:45 -0400

    First passing TickedOffTest scenario

--- OpenAgile.NuGetPluginManager.Tests/MangePlugins.Steps.fs
+++ OpenAgile.NuGetPluginManager.Tests/MangePlugins.Steps.fs
@@ -26,17 +26,33 @@ let makePackages (plugins : string) =
                     .Create()
             pkg
         ] |> Queryable.AsQueryable
+
+let makePluginManager installedPackages availablePackages =
+    let remotePackagegManager = Mock<NuGet.IPackageManager>().Create()
+    let localPackageManager = Mock<NuGet.IPackageManager>().Create()
+    NuGetPluginManager("", "", "MyApp.Awesome.Plugin."
+        , (fun () -> availablePackages )
+        , (fun (pattern, allowPrelease) -> query {
+                for package in availablePackages do
+                where (package.Id.StartsWith pattern)
+                select package
+            })
+        , remotePackagegManager, localPackageManager)
         
 type State = { 
     AvailablePackages : IQueryable<NuGet.IPackage>
     InstalledPackages : IQueryable<NuGet.IPackage>
-    //PluginManager : NuGetPluginManager
+    PluginManager : NuGetPluginManager
   }
-    with static member Create () = { 
-            AvailablePackages = makePackages String.Empty
-            InstalledPackages = makePackages String.Empty
-            //PluginManager = null
-        }
+    with static member Create () = 
+            let availablePackages = makePackages String.Empty
+            let installedPackages = makePackages String.Empty
+            let state = { 
+                AvailablePackages = availablePackages
+                InstalledPackages = installedPackages
+                PluginManager = makePluginManager installedPackages availablePackages
+            }
+            state
 
 let performStep (state:State) (step, line:LineSource) =
     match step with
@@ -44,10 +60,17 @@ let performStep (state:State) (step, line:LineSource) =
         { state with AvailablePackages = makePackages pluginList }
     | Given "no plugins currently installed" [] ->
         { state with InstalledPackages = makePackages String.Empty }
-//    | When "requesting the list of available plugins" [] ->
-//        { state with }
-  
-
+    | When "requesting the list of available plugins" [] ->
+        { state with PluginManager = makePluginManager state.InstalledPackages state.AvailablePackages  }
+    | Then "I should see (.*)" [pluginList] ->
+        let expectedPlugins = makePackages pluginList
+        // TODO better way:
+        let pairs = List.zip (List.ofSeq expectedPlugins) (List.ofSeq state.AvailablePackages)
+        for item in pairs do
+            let expected, actual = item
+            NUnit.Framework.Assert.AreEqual(expected.Id, actual.Id)
+            NUnit.Framework.Assert.AreEqual(expected.Version, actual.Version)
+        state
     | _ -> sprintf "Unmatched line %d" line.Number |> invalidOp
 
 [<TestFixture>]
