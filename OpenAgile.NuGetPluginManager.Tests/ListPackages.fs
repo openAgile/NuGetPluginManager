75b5b6f40be7ea1ebc884513a6e7f4bb27eb3a73
Josh Gough <jsgough@gmail.com>
2014-03-20 19:20:51 -0400

    Rename variable for clarity

--- OpenAgile.NuGetPluginManager.Tests/ListPackages.fs
+++ OpenAgile.NuGetPluginManager.Tests/ListPackages.fs
@@ -18,21 +18,21 @@ type ListPackages_when_two_versions_of_the_same_package_are_in_remote_repository
     
     let subject = makePluginManager installedPackages availablePackages
 
-    let foundPackages = subject.ListPackages()
+    let groupedPackages = subject.ListPackages()
 
     [<Test>]
     member t.found_one_grouping() =
-        Assert.AreEqual(1, foundPackages.Count)
+        Assert.AreEqual(1, groupedPackages.Count)
 
     [<Test>]
     member t.found_correct_package() =
-        let packageGroup = foundPackages.[packageId]
+        let packageGroup = groupedPackages.[packageId]
         let package = Seq.nth 0 packageGroup
         Assert.AreEqual(packageId, package.Id)
 
     [<Test>]
     member t.packages_sorted_with_most_recent_at_top() =
-        let packageGroup = foundPackages.[packageId]
+        let packageGroup = groupedPackages.[packageId]
         let v2 = Seq.nth 0 packageGroup
         let v1 = Seq.nth 1 packageGroup
         Assert.AreEqual(newestVersion, v2.VersionString)
