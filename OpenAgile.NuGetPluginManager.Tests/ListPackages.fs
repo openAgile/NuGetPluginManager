17f1a79c099162524957fbc2eaacae572e8a7d89
Josh Gough <jsgough@gmail.com>
2014-03-21 19:30:29 -0400

    Refactored to use FSunit and use NUnit's in-bult collection ordering assertion support

--- OpenAgile.NuGetPluginManager.Tests/ListPackages.fs
+++ OpenAgile.NuGetPluginManager.Tests/ListPackages.fs
@@ -1,23 +1,18 @@
 ﻿namespace NuGetPluginManager
 
 module public ListPackages =
-
     open NUnit.Framework
     open System.Linq
     open System.Collections
     open System
     open NuGetPluginManagerSetup
+    open FsUnit
 
     let newestVersion = "100.0.0.0"
     let oldestVersion = "20.0.0.0"
 
-    let AssertPackageOrderCorrect (packageGroup : IOrderedEnumerable<OpenAgile.NuGetPluginManager.PackageModel>) =
-        // Test BinFoo ordering
-        let v2 = Seq.nth 0 packageGroup
-        let v1 = Seq.nth 1 packageGroup
-
-        Assert.AreEqual(newestVersion, v2.VersionString)
-        Assert.AreEqual(oldestVersion, v1.VersionString)
+    let should_be_ordered_descending_by_SemanticVersion (packageGroup : IOrderedEnumerable<OpenAgile.NuGetPluginManager.PackageModel>) =
+        Assert.That(packageGroup, Is.Ordered.Descending.By("SemanticVersion"))
 
     [<TestFixture>]
     [<Description("Unit test of the plugin manager when multiple versions of a single package exist in the remote NuGet Repository")>]
@@ -25,26 +20,25 @@ module public ListPackages =
         // Implicit setup?
         let packageId = "MyApp.Awesome.Plugin.BinFoo"
 
-        let availablePackages = makePackages (sprintf "%s|%s, %s|%s" packageId newestVersion packageId oldestVersion)
-        let installedPackages = makePackages String.Empty
+        let availablePackages = sprintf "%s|%s, %s|%s" packageId newestVersion packageId oldestVersion |> makePackages
+        let installedPackages = String.Empty |> makePackages
     
         let subject = makePluginManager installedPackages availablePackages
 
         let groupedPackages = subject.ListPackages()
 
         [<Test>]
-        member It.``returns one package group``() =
-            Assert.AreEqual(1, groupedPackages.Count)
+        member It.``returns one package group``() = groupedPackages.Count |> should equal 1
 
         [<Test>]
         member It.``returns the correct package group``() =
             let packageGroup = groupedPackages.[packageId]
             let package = Seq.nth 0 packageGroup
-            Assert.AreEqual(packageId, package.Id)
+            package.Id |> should equal packageId
 
         [<Test>]
-        member It.``returns the distinct package versions with most recent first``() =
-            AssertPackageOrderCorrect groupedPackages.[packageId]
+        member It.``returns packages ordered descending by SemanticVersion``() =
+            groupedPackages.[packageId] |> should_be_ordered_descending_by_SemanticVersion
 
     [<TestFixture>]
     type ``when two versions of a different package are in remote repository``() =
@@ -53,19 +47,18 @@ module public ListPackages =
         let packageFubarId = "MyApp.Awesome.Plugin.Fubar"
 
         let availablePackages = 
-            makePackages (sprintf "%s|%s, %s|%s, %s|%s, %s|%s" 
+            sprintf "%s|%s, %s|%s, %s|%s, %s|%s" 
                 packageBinFooId newestVersion packageBinFooId oldestVersion 
-                packageFubarId newestVersion packageFubarId oldestVersion)
+                packageFubarId newestVersion packageFubarId oldestVersion |> makePackages
     
-        let installedPackages = makePackages String.Empty
+        let installedPackages = String.Empty |> makePackages
     
         let subject = makePluginManager installedPackages availablePackages
 
         let groupedPackages = subject.ListPackages()
 
         [<Test>]
-        member It.``returns two package groups``() =
-            Assert.AreEqual(2, groupedPackages.Count)
+        member It.``returns two package groups``() = groupedPackages |> should equal 2
 
         [<Test>]
         member It.``returns the correct package groups``() =
@@ -73,12 +66,11 @@ module public ListPackages =
             let packageGroupFubar = groupedPackages.[packageFubarId]
             let packageBinFoo = Seq.nth 0 packageGroupBinFoo
             let packageFubar = Seq.nth 1 packageGroupFubar
-            Assert.AreEqual(packageBinFooId, packageBinFoo.Id)
-            Assert.AreEqual(packageFubarId, packageFubar.Id)
+            packageBinFoo.Id |> should equal packageBinFooId
+            packageFubar.Id |> should equal packageFubarId
+
 
         [<Test>]
-        member It.``returns the distinct package versions with most recent first``() =
-            // Test BinFoo ordering
-            AssertPackageOrderCorrect groupedPackages.[packageBinFooId]
-            // Test Fubar ordering
-            AssertPackageOrderCorrect groupedPackages.[packageFubarId]
+        member It.``returns packages ordered descending by SemanticVersion``() =
+            groupedPackages.[packageBinFooId] |> should_be_ordered_descending_by_SemanticVersion
+            groupedPackages.[packageFubarId] |> should_be_ordered_descending_by_SemanticVersion
